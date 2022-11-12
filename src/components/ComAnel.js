038380d42c07b4c3bc8298abe2a1e3087862b0a3
Israel-Felipe <israelfelipet@gmail.com>
2022-11-12 11:22:25 -0300

    efeito vidro

--- src/components/ComAnel.js
+++ src/components/ComAnel.js
@@ -128,14 +128,18 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: rgba(255, 255, 255, 0.9);
-  border-radius: 20px;
   position: fixed;
   top: 2.5vh;
   left: 5vw;
   width: 90vw;
   height: 95vh;
   color: rgba(114, 115, 118);
+  background: rgba(255, 255, 255, 0.35);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
 
   @media (min-width: 1300px) {
     flex-direction: initial;
--- src/components/Home.js
+++ src/components/Home.js
@@ -34,8 +34,12 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   gap: 20px;
-  background-color: rgba(255, 255, 255, 0.9);
-  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.35);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(13.5px);
+  -webkit-backdrop-filter: blur(13.5px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
 
   @media (min-width: 1500px) {
     flex-direction: inherit;
